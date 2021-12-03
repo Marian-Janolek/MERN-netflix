b4a94138eca227fd10a192c876720d26716f9e29
Marian-Janolek <m.janolek@gmail.com>
2021-12-03 13:37:18 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+connectDB = mongoose.connect;
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+      .then(() => console.log('Successful connected to DB'))
+      .catch((err) => console.log(err));
+    app.listen(port, console.log(`Server is running on port : ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Home from './pages/home/Home';
+import './app.scss';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
