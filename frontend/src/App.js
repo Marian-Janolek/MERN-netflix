24dcb4b90a3305cf493581e20cccf93e3b2c6b82
Marian-Janolek <m.janolek@gmail.com>
2021-12-04 13:50:39 +0100

    add react links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,43 @@ import Home from './pages/home/Home';
 import './app.scss';
 import Register from './pages/register/Register';
 import Login from './pages/login/Login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import Watch from './pages/watch/Watch';
 
 function App() {
+  const user = false;
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Redirect to="/register" />}
+        </Route>
+        <Route exact path="/register">
+          {!user ? <Register /> : <Redirect to="/" />}
+        </Route>
+        <Route exact path="/login">
+          {!user ? <Login /> : <Redirect to="/" />}
+        </Route>
+        {user && (
+          <>
+            <Route path="/movies">
+              <Home type="movies" />
+            </Route>
+            <Route path="/series">
+              <Home type="series" />
+            </Route>
+            <Route path="/watch">
+              <Watch />
+            </Route>
+          </>
+        )}
+      </Switch>
+    </Router>
   );
 }
 
